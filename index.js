7ccde8873c57a771ff234db9b1c3cc1699ea1c7d
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-18 23:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ $(document).ready(function (){
 
 function searchRepositories() {
   let searchTerms = $('#searchTerms').val();
-  $.get(`https://api.github.com/search/repositories?q=user:${searchTerms}`, function(data) {
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
     console.log(data.items[0]);
     let r = `${data.items[0]}`;
     document.getElementById('results').innerHTML = r;
