76bf50409cb01d1eccd841491a38b79cdae43189
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-18 23:24:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ $(document).ready(function (){
 function searchRepositories() {
   let searchTerms = $('#searchTerms').val();
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
-    console.log(data.items[0]);
-    let r = `${data.items[0]}`;
-    document.getElementById('results').innerHTML = r;
+    $('#results').html(renderSearchResults(data));
   });
 }
+
+var renderSearchResult = (result) => {
+  return `
+      <div>
+        <h2><a href="${result.html_url}">${result.name}</a></h2>
+        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+        <p>${result.description}</p>
+      </div>
+      <hr>
+    `
+}
+
+var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
