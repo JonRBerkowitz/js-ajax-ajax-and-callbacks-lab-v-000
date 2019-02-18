7da8d53c95357eb00c4a63b61773066de8f5b6ed
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-18 23:42:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,18 @@ function searchRepositories() {
   });
 }
 
-var renderSearchResult = (result) => {
-  return `
-      <div>
-        <h2><a href="${result.html_url}">${result.name}</a></h2>
-        <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-        <p>${result.description}</p>
-      </div>
-      <hr>
-    `
+function renderSearchResults(data) {
+  let result = data.items.map(r => {
+    `<div>
+      <h2><a href="${result.html_url}">${result.name}</a></h2>
+      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      <p>${result.description}</p>
+    </div>
+    <hr>`
+  });
+  return result;
 }
 
-var renderSearchResults = (data) => data.items.map( result => renderSearchResult(result))
+function renderSearchResults(data) {
+  data.items.map( result => renderSearchResult(result))
+} 
