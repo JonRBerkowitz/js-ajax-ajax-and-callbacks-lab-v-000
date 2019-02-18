bc514c94c1110aecd9c5dda1c5ebe027665fef60
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-18 23:15:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 $(document).ready(function (){
-  function searchRepositories() {
-    let searchTerms = $('#searchTerms').val();
-    $.get(`https://api.github.com/search/repositories?q=user:${searchTerms}`, function(data) {
-      console.log(data.items[0]);
-      $('#results').html(data.items[0]);
-    });
-  }
 });
+
+function searchRepositories() {
+  let searchTerms = $('#searchTerms').val();
+  $.get(`https://api.github.com/search/repositories?q=user:${searchTerms}`, function(data) {
+    console.log(data.items[0]);
+    let r = data.items[0];
+    $('#results').html(r);
+  });
+}
