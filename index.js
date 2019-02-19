8432805969189eb7d06bdb9bcf74ece4c4189604
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-19 00:17:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ function searchRepositories() {
 function renderSearchResults(data) {
   let results = data.items.map(result => {
     return
-    `<div>
-      <h2><a href="${result.html_url}">${result.name}</a></h2>
-      <p><a href="#" data-repository="${result.name}" data-owner="${result.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
-      <p>${result.description}</p>
-    </div>
-    <hr>`
+    `${result.name}`
   });
-  return results;
   console.log(results);
 }
